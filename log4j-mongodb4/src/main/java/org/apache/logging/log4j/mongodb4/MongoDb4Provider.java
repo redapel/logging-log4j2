675f4e6e40f2cddd2a3c13eb7d6ac273c869b5e1
redapel <apelgrannysmith@outlook.com>
2021-08-21 17:46:08 +0000

    LOG4J2-3122 fix Logging to Log4j2 MongoDb4 Appender get ERROR on trace, debug and info level.

--- log4j-mongodb4/src/main/java/org/apache/logging/log4j/mongodb4/MongoDb4Provider.java
+++ log4j-mongodb4/src/main/java/org/apache/logging/log4j/mongodb4/MongoDb4Provider.java
@@ -41,92 +41,114 @@ import com.mongodb.client.MongoDatabase;
 @Plugin(name = "MongoDb4", category = Core.CATEGORY_NAME, printObject = true)
 public final class MongoDb4Provider implements NoSqlProvider<MongoDb4Connection> {
 
-    public static class Builder<B extends Builder<B>> extends AbstractFilterable.Builder<B>
-            implements org.apache.logging.log4j.core.util.Builder<MongoDb4Provider> {
+	public static class Builder<B extends Builder<B>> extends AbstractFilterable.Builder<B>
+			implements org.apache.logging.log4j.core.util.Builder<MongoDb4Provider> {
 
-        @PluginBuilderAttribute(value = "connection")
-        @Required(message = "No connection string provided")
-        private String connection;
+		@PluginBuilderAttribute(value = "connection")
+		@Required(message = "No connection string provided")
+		private String connection;
 
-        @PluginBuilderAttribute
-        private int collectionSize = DEFAULT_COLLECTION_SIZE;
+		@PluginBuilderAttribute
+		private int collectionSize = DEFAULT_COLLECTION_SIZE;
 
-        @PluginBuilderAttribute("capped")
-        private boolean capped = false;
+		@PluginBuilderAttribute("capped")
+		private boolean capped = false;
 
-        @Override
-        public MongoDb4Provider build() {
-            return new MongoDb4Provider(connection, capped, collectionSize);
-        }
+		@Override
+		public MongoDb4Provider build() {
+			ConnectionString connectionString;
+			MongoClient mongoClient = null;
+			MongoDatabase mongoDatabase;
 
-        public B setCapped(final boolean isCapped) {
-            this.capped = isCapped;
-            return asBuilder();
-        }
+			LOGGER.debug("Creating ConnectionString {}...", connection);
+			connectionString = new ConnectionString(connection);
+			LOGGER.debug("Created ConnectionString {}", connectionString);
+			LOGGER.debug("Creating MongoClientSettings...");
+			// @formatter:off
+            final MongoClientSettings settings = MongoClientSettings.builder()
+                    .applyConnectionString(connectionString)
+                    .codecRegistry(CODEC_REGISTRIES)
+                    .build();
+            // @formatter:on
+			LOGGER.debug("Created MongoClientSettings {}", settings);
+			LOGGER.debug("Creating MongoClient {}...", settings);
+			mongoClient = MongoClients.create(settings);
+			LOGGER.debug("Created MongoClient {}", mongoClient);
+			String databaseName = connectionString.getDatabase();
+			LOGGER.debug("Getting MongoDatabase {}...", databaseName);
+			mongoDatabase = mongoClient.getDatabase(databaseName);
+			LOGGER.debug("Got MongoDatabase {}", mongoDatabase);
+			try {
+				mongoDatabase.listCollectionNames().first(); // Check if the database actually requires authentication
+			} catch (final Exception e) {
+				LOGGER.error(
+						"The database is not up, or you are not authenticated, try supplying a username and password to the MongoDB provider.",
+						e);
+				close(mongoClient);
+				return null;
+			}
+			return new MongoDb4Provider(connectionString, mongoClient, mongoDatabase, capped, collectionSize);
+		}
 
-        public B setCollectionSize(final int collectionSize) {
-            this.collectionSize = collectionSize;
-            return asBuilder();
-        }
-    }
+		private void close(final MongoClient mongoClient) {
+			if (mongoClient != null) {
+				mongoClient.close();
+			}
+		}
 
-    private static final Logger LOGGER = StatusLogger.getLogger();
+		public B setCapped(final boolean isCapped) {
+			this.capped = isCapped;
+			return asBuilder();
+		}
 
-    // @formatter:off
+		public B setCollectionSize(final int collectionSize) {
+			this.collectionSize = collectionSize;
+			return asBuilder();
+		}
+	}
+
+	private static final Logger LOGGER = StatusLogger.getLogger();
+
+	// @formatter:off
     private static final CodecRegistry CODEC_REGISTRIES = CodecRegistries.fromRegistries(
             MongoClientSettings.getDefaultCodecRegistry(),
             CodecRegistries.fromCodecs(MongoDb4LevelCodec.INSTANCE));
     // @formatter:on
 
-    // TODO Where does this number come from?
-    private static final int DEFAULT_COLLECTION_SIZE = 536_870_912;
+	// TODO Where does this number come from?
+	private static final int DEFAULT_COLLECTION_SIZE = 536_870_912;
 
-    @PluginBuilderFactory
-    public static <B extends Builder<B>> B newBuilder() {
-        return new Builder<B>().asBuilder();
-    }
+	@PluginBuilderFactory
+	public static <B extends Builder<B>> B newBuilder() {
+		return new Builder<B>().asBuilder();
+	}
 
-    private final Integer collectionSize;
-    private final boolean isCapped;
-    private final MongoClient mongoClient;
-    private final MongoDatabase mongoDatabase;
-    private final ConnectionString connectionString;
+	private final Integer collectionSize;
+	private final boolean isCapped;
+	private final MongoClient mongoClient;
+	private final MongoDatabase mongoDatabase;
+	private final ConnectionString connectionString;
 
-    private MongoDb4Provider(final String connectionStringSource, final boolean isCapped,
-            final Integer collectionSize) {
-        LOGGER.debug("Creating ConnectionString {}...", connectionStringSource);
-        this.connectionString = new ConnectionString(connectionStringSource);
-        LOGGER.debug("Created ConnectionString {}", connectionString);
-        LOGGER.debug("Creating MongoClientSettings...");
-        // @formatter:off
-        final MongoClientSettings settings = MongoClientSettings.builder()
-                .applyConnectionString(this.connectionString)
-                .codecRegistry(CODEC_REGISTRIES)
-                .build();
-        // @formatter:on
-        LOGGER.debug("Created MongoClientSettings {}", settings);
-        LOGGER.debug("Creating MongoClient {}...", settings);
-        this.mongoClient = MongoClients.create(settings);
-        LOGGER.debug("Created MongoClient {}", mongoClient);
-        String databaseName = this.connectionString.getDatabase();
-        LOGGER.debug("Getting MongoDatabase {}...", databaseName);
-        this.mongoDatabase = this.mongoClient.getDatabase(databaseName);
-        LOGGER.debug("Got MongoDatabase {}", mongoDatabase);
-        this.isCapped = isCapped;
-        this.collectionSize = collectionSize;
-    }
+	private MongoDb4Provider(final ConnectionString connectionString, final MongoClient mongoClient,
+			final MongoDatabase mongoDatabase, final boolean isCapped, final Integer collectionSize) {
+		this.connectionString = connectionString;
+		this.mongoClient = mongoClient;
+		this.mongoDatabase = mongoDatabase;
+		this.isCapped = isCapped;
+		this.collectionSize = collectionSize;
+	}
 
-    @Override
-    public MongoDb4Connection getConnection() {
-        return new MongoDb4Connection(connectionString, mongoClient, mongoDatabase, isCapped, collectionSize);
-    }
+	@Override
+	public MongoDb4Connection getConnection() {
+		return new MongoDb4Connection(connectionString, mongoClient, mongoDatabase, isCapped, collectionSize);
+	}
 
-    @Override
-    public String toString() {
-        return String.format(
-                "%s [connectionString=%s, collectionSize=%s, isCapped=%s, mongoClient=%s, mongoDatabase=%s]",
-                MongoDb4Provider.class.getSimpleName(), connectionString, collectionSize, isCapped, mongoClient,
-                mongoDatabase);
-    }
+	@Override
+	public String toString() {
+		return String.format(
+				"%s [connectionString=%s, collectionSize=%s, isCapped=%s, mongoClient=%s, mongoDatabase=%s]",
+				MongoDb4Provider.class.getSimpleName(), connectionString, collectionSize, isCapped, mongoClient,
+				mongoDatabase);
+	}
 
 }
